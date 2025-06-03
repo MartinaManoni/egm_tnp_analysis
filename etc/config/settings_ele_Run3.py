2f2d0ec8fde1395d8dd4a49e9d0164e6d88cc463
Martina Manoni <m.manoni2@campus.unimib.it>
2025-06-03 10:01:24 +0200

    Update settings with new input samples, remove all tagSel instances, add binning + base and additional cuts by Andro

--- etc/config/settings_ele_Run3.py
+++ etc/config/settings_ele_Run3.py
@@ -18,9 +18,11 @@ flags = {
     'passingMVA94XwpLisoV2'    : '(passingMVA94XwpLisoV2 == 1)',
     'passingMVA94XwpLnoisoV2'  : '(passingMVA94XwpLnoisoV2 == 1)',
     'passingMVA94XwpHZZisoV2'  : '(passingMVA94XwpHZZisoV2 == 1)',
+    'passingMVAhzzWinter22'  : '(passingMVAhzzWinter22 == 1)', #Martina new ID hzzWinter22 with 22 training
+    'passingMVASummer18ULwpHZZ_sipdzdxy' : '(passingMVASummer18ULwpHZZ == 1 && fabs(el_sip) < 4 && fabs(el_dz) < 1 && fabs(el_dxy) < 0.5)',#Andro 2022 with UL18 training
     }
 
-baseOutDir = 'results/Run3_BCDEFG/tnpEleID/'
+baseOutDir = '/eos/user/m/mmanoni/Tnp_results/hzzWinter22/'
 
 #############################################################
 ########## samples definition  - preparing the samples
@@ -31,18 +33,18 @@ import etc.inputs.tnpSampleDef as tnpSamples
 tnpTreeDir = 'tnpEleIDs'
 
 samplesDef = {
-    'data'   : tnpSamples.Run3['data_Run3B'].clone(),
-    'mcNom'  : tnpSamples.Run3['DY_madgraph'].clone(),
+    'data'   : tnpSamples.Run3_2022['data_Run2022F'].clone(),
+    'mcNom'  : tnpSamples.Run3_2022['DYpostEE22'].clone(),
     #'mcAlt'  : tnpSamples.Run3['DY_amcatnloext'].clone(),
-    'tagSel' : tnpSamples.Run3['DY_madgraph'].clone(),
+    #'tagSel' : tnpSamples.Run3['DY_madgraph'].clone(),
 }
 
 ## can add data sample easily
-samplesDef['data'].add_sample( tnpSamples.Run3['data_Run3C'] )
-samplesDef['data'].add_sample( tnpSamples.Run3['data_Run3D'] )
-samplesDef['data'].add_sample( tnpSamples.Run3['data_Run3E'] )
-samplesDef['data'].add_sample( tnpSamples.Run3['data_Run3F'] )
-samplesDef['data'].add_sample( tnpSamples.Run3['data_Run3G'] )
+samplesDef['data'].add_sample( tnpSamples.Run3_2022['data_Run2022G'] )
+#samplesDef['data'].add_sample( tnpSamples.Run3_Andro['data_Run3D'] )
+#samplesDef['data'].add_sample( tnpSamples.Run3_Andro['data_Run3E'] )
+#samplesDef['data'].add_sample( tnpSamples.Run3_Andro['data_Run3F'] )
+#samplesDef['data'].add_sample( tnpSamples.Run3_Andro['data_Run3G'] )
 
 ## some sample-based cuts... general cuts defined here after
 ## require mcTruth on MC DY samples and additional cuts
@@ -52,32 +54,34 @@ samplesDef['data'].add_sample( tnpSamples.Run3['data_Run3G'] )
 samplesDef['data' ].set_tnpTree(tnpTreeDir)
 if not samplesDef['mcNom' ] is None: samplesDef['mcNom' ].set_tnpTree(tnpTreeDir)
 #if not samplesDef['mcAlt' ] is None: samplesDef['mcAlt' ].set_tnpTree(tnpTreeDir)
-if not samplesDef['tagSel'] is None: samplesDef['tagSel'].set_tnpTree(tnpTreeDir)
+#if not samplesDef['tagSel'] is None: samplesDef['tagSel'].set_tnpTree(tnpTreeDir)
 
 if not samplesDef['mcNom' ] is None: samplesDef['mcNom' ].set_mcTruth()
 #if not samplesDef['mcAlt' ] is None: samplesDef['mcAlt' ].set_mcTruth()
-if not samplesDef['tagSel'] is None: samplesDef['tagSel'].set_mcTruth()
-if not samplesDef['tagSel'] is None:
-    samplesDef['tagSel'].rename('mcAltSel_DY_madgraph')
-    samplesDef['tagSel'].set_cut('tag_Ele_pt > 37') #canceled non trig MVA cut
+#if not samplesDef['tagSel'] is None: samplesDef['tagSel'].set_mcTruth()
+#if not samplesDef['tagSel'] is None:
+    #samplesDef['tagSel'].rename('mcAltSel_DY_madgraph')
+    #samplesDef['tagSel'].set_cut('tag_Ele_pt > 37') #canceled non trig MVA cut
 
 
 ## set MC weight, can use several pileup rw for different data taking periods
 #weightName = 'weights_data_Run2022_B-G.totWeight'
+# TODO: verify these MC weights are correct
 weightName = 'weights_data_Run2022_inclusive.totWeight'
 if not samplesDef['mcNom' ] is None: samplesDef['mcNom' ].set_weight(weightName)
 #if not samplesDef['mcAlt' ] is None: samplesDef['mcAlt' ].set_weight(weightName)
-if not samplesDef['tagSel'] is None: samplesDef['tagSel'].set_weight(weightName)
+#if not samplesDef['tagSel'] is None: samplesDef['tagSel'].set_weight(weightName)
 if not samplesDef['mcNom' ] is None: samplesDef['mcNom' ].set_puTree('/eos/cms/store/group/phys_egamma/ec/fmausolf/EGM_comm/DYToEE_M-50_NNPDF31_TuneCP5_13p6TeV-powheg-pythia8_EleID_PhoID/DYToEE_M-50_NNPDF31_TuneCP5_13p6TeV-powheg-pythia8/DYToEE_M-50_NNPDF31_TuneCP5_13p6TeV-powheg-pythia8_EleID_PhoID/221018_080249/0000/DY_powheg_ele.pu.puTree.root')
 #if not samplesDef['mcAlt' ] is None: samplesDef['mcAlt' ].set_puTree('/eos/cms/store/group/phys_egamma/swmukher/UL2017/PU_miniAOD/DY_amcatnloext_ele.pu.puTree.root')
-if not samplesDef['tagSel'] is None: samplesDef['tagSel'].set_puTree('/eos/cms/store/group/phys_egamma/ec/fmausolf/EGM_comm/DYToEE_M-50_NNPDF31_TuneCP5_13p6TeV-powheg-pythia8_EleID_PhoID/DYToEE_M-50_NNPDF31_TuneCP5_13p6TeV-powheg-pythia8/DYToEE_M-50_NNPDF31_TuneCP5_13p6TeV-powheg-pythia8_EleID_PhoID/221018_080249/0000/DY_powheg_ele.pu.puTree.root')
+#if not samplesDef['tagSel'] is None: samplesDef['tagSel'].set_puTree('/eos/cms/store/group/phys_egamma/ec/fmausolf/EGM_comm/DYToEE_M-50_NNPDF31_TuneCP5_13p6TeV-powheg-pythia8_EleID_PhoID/DYToEE_M-50_NNPDF31_TuneCP5_13p6TeV-powheg-pythia8/DYToEE_M-50_NNPDF31_TuneCP5_13p6TeV-powheg-pythia8_EleID_PhoID/221018_080249/0000/DY_powheg_ele.pu.puTree.root')
 
 #############################################################
 ########## bining definition  [can be nD bining]
 #############################################################
+# Based on Andro binning 
 biningDef = [
-   { 'var' : 'el_sc_eta' , 'type': 'float', 'bins': [-2.5,-2.0,-1.566,-1.4442, -0.8, 0.0, 0.8, 1.4442, 1.566, 2.0, 2.5] },
-   { 'var' : 'el_pt' , 'type': 'float', 'bins': [10,20,35,50,100,500] },
+    { 'var' : 'el_sc_eta' , 'type': 'float', 'bins': [-2.5, -2.0, -1.566, -0.8, 0, 0.8, 1.566, 2.0, 2.5] },
+    { 'var' : 'el_pt' , 'type': 'float', 'bins': [7, 15, 20, 35, 50, 100, 500] },
 ]
 
 #############################################################
@@ -87,16 +91,14 @@ biningDef = [
 cutBase   = 'tag_Ele_pt > 35 && abs(tag_sc_eta) < 2.17 && el_q*tag_Ele_q < 0'
 
 additionalCuts = { 
-    0 : 'tag_Ele_trigMVA > 0.92 ',
-    1 : 'tag_Ele_trigMVA > 0.92 ',
-    2 : 'tag_Ele_trigMVA > 0.92 ',
-    3 : 'tag_Ele_trigMVA > 0.92 ',
-    4 : 'tag_Ele_trigMVA > 0.92 ',
-    5 : 'tag_Ele_trigMVA > 0.92 ',
-    6 : 'tag_Ele_trigMVA > 0.92 ',
-    7 : 'tag_Ele_trigMVA > 0.92 ',
-    8 : 'tag_Ele_trigMVA > 0.92 ',
-    9 : 'tag_Ele_trigMVA > 0.92 '
+    0 : 'tag_Ele_pt > 50 ',
+    1 : 'tag_Ele_pt > 50 ',
+    2 : 'tag_Ele_pt > 50 ',
+    3 : 'tag_Ele_pt > 50 ',
+    4 : 'tag_Ele_pt > 50 ',
+    5 : 'tag_Ele_pt > 50 ',
+    6 : 'tag_Ele_pt > 50 ',
+    7 : 'tag_Ele_pt > 50 '
 }
 
 #### or remove any additional cut (default)
@@ -133,3 +135,30 @@ tnpParAltBkgFit = [
     "alphaP[0.,-5.,5.]",
     "alphaF[0.,-5.,5.]",
     ]
+
+tnpParAltSigBkgFit = [
+    #"meanP[-0.0,-5.0,5.0]","sigmaP[1,0.7,6.0]","alphaP[2.0,0.0,3.5]" ,'nP[3,-5,5]',"sigmaP_2[1.5,0.5,6.0]","sosP[1,0.1,5.0]", #works for most bins
+    "meanP[-2.08,-3.,2.]","sigmaP[1,0.7,6.0]","alphaP[2.0,0.0,2.0]" ,'nP[3,-5,5]',"sigmaP_2[1.5,0.5,6.0]","sosP[1.487,0.,3.]",
+    #"meanP[-1.3,-1.4,-0.2]","sigmaP[1,0.1,3.0]","alphaP[1.2,1.0,5.0]" ,'nP[3,-5,5]',"sigmaP_2[1.5,0.5,6.0]","sosP[1.4,1.3,1.5]",
+    "meanF[-2.195,-3.0,-1.0]","sigmaF[4.786,3.,6.]","alphaF[0.72,0,1.5]",'nF[1.734,1.0,2.5]',"sigmaF_2[2.0,1.0,3.0]","sosF[2.0,1.0,3.0]", #bins 0,1,2,5,6,7
+    #"meanF[-0.974,-1.3,-0.9]","sigmaF[4.668,4.2,5.2]","alphaF[1.376,0.8,2.0]",'nF[0.236,0.1,0.4]',"sigmaF_2[2.0]","sosF[2.0]", 
+    #"meanF[-2.195,-3.0,-0.0]","sigmaF[4.786,3.,6.]","alphaF[0.72,0,3.5]",'nF[1.734,1.0,2.5]',"sigmaF_2[2.0,1.0,3.0]","sosF[2.0,0.4,3.0]", #bins 3,4
+    #"meanF[-2.754,-3.5,0.0]","sigmaF[3.871,3.2,4.6]","alphaF[1.873,1.0,2.8]",'nF[0.163]',"sigmaF_2[2.0]","sosF[2.863,0.0,3.7]", #bins 8,9 14,15
+    #"meanF[-0.155,-0.2,-0.1]","sigmaF[0.702,0.4,1.0]","alphaF[1.116,1.0,1.3]",'nF[0.77,0.0.1.5]',"sigmaF_2[2.0,1.5,2.5]","sosF[1.477,1.2,1.7]", #bins 10,13
+    #"meanF[-2.754,-3.5,0.0]","sigmaF[2.4,2.0,3.5]","alphaF[1.873,1.0,2.8]",'nF[0.071,0,0.2]',"sigmaF_2[2.0]","sosF[2.6,0.0,3.7]", #bins 8,9, 14,15,11,12
+    #"meanF[-2.754,-3.5,0.0]","sigmaF[2.4,2.0,3.5]","alphaF[1.873,1.0,2.8]",'nF[0.071,0,0.2]',"sigmaF_2[2.0]","sosF[2.6,0.0,3.7]", #bins 8,9, 14,15,11,12
+    #"meanF[-2.754,-3.5,1.0]","sigmaF[2.4,2.0,3.5]","alphaF[1.873,1.0,6.8]",'nF[0.071,0,0.2]',"sigmaF_2[2.0]","sosF[2.6,0.0,3.7]", #bins 16-23
+    #"meanF[-0.0,-5.0,5.0]","sigmaF[2,0.7,6.0]","alphaF[2.0,1.2,3.5]",'nF[3,-5,5]',"sigmaF_2[2.0,0.5,6.0]","sosF[1,0.5,5.0]",
+    "alphaP_2[0.,-5.,5.]",
+    "alphaF_2[0,-1, 1]",
+    #"alpha2F[-0.033,-0.036,-0.03]", #bins 10,13
+    #"alpha2F[-0.028,-0.1,0.0]", #bins 8,9,14,15
+    #"a0[1.87,1.7,7.5]","a1[14.7,13.7,15.7]","a2[30,28,62]",
+    #"a0[2.0,1.9,2.4]","a1[15.7,13.7,18.7]","a2[30,28,62]", #bins 11,12
+    #"a0[0.25,0.1,0.35]","a1[140,135,160]","a2[40,35,75]", #bins 0,1,6,7
+    #"a0[0.1,0.01,0.5]","a1[120,80,160]","a2[41.829,36,48]",
+    #"a0[0.25,0.1,0.35]","a1[70,60,80]","a2[37,35,75]", #bins 2,5
+    #"a0[1.7,1.0,2.5]","a1[15.7,12.7,18.7]","a2[32,20,44]", #bins 3,4
+    #"a0[1.87,1.7,7.5]","a1[15.7,14.7,40.7]","a2[30,28,62]",
+    #"a0[1.3,1.2,1.45]","a1[21.7,20.7,29.7]","a2[55.6,51.6,57.6]", #bins 16-23
+]
--- etc/inputs/tnpSampleDef.py
+++ etc/inputs/tnpSampleDef.py
@@ -25,7 +25,7 @@ eos_Run3_PromptReco = '/eos/cms/store/group/phys_egamma/tnpTuples/bjoshi/2023-04
 eos_Reco_Run3_PromptReco = '/eos/cms/store/group/phys_egamma/tnpTuples/bjoshi/2023-05-01/2022/data/'
 eos_Reco_Run3_124X = '/eos/cms/store/group/phys_egamma/tnpTuples/bjoshi/2023-02-09/2022/mc/'
 
-
+eos_marti = '/eos/user/m/mmanoni/Final_TnpNtuples/'
 ReReco2017 = {
 
     'DY_madgraph'              : tnpSample('DY_madgraph',
@@ -196,4 +196,30 @@ Run3_Reco_124X_PromptReco_postEE = {
         'data_Run2022E': tnpSample('data_Run2022E', eos_Reco_Run3_PromptReco + '/TnPTree_EGamma_2022_Run2022E.root', lumi = 5.8784),
         'data_Run2022F': tnpSample('data_Run2022F', eos_Reco_Run3_PromptReco + '/TnPTree_EGamma_2022_Run2022F.root', lumi = 18.0064),
         'data_Run2022G': tnpSample('data_Run2022G', eos_Reco_Run3_PromptReco + '/TnPTree_EGamma_2022_Run2022G.root', lumi = 3.1219)
+        }
+
+# Martina samples 2022+2023 #TODO: Complete this section, for now working only for 2022postEE
+Run3_2022 = {
+        #'DYpreEE'    : tnpSample('', eos_marti + 'TnPTree_DYJetsToLL_M-50_TuneCP5_13p6TeV-madgraphMLM-pythia8_2022_DY_LO_postEE.root', isMC=True, nEvts = -1),
+        'DYpostEE22'    : tnpSample('DYpostEE22', eos_marti + '/2022postEE/TnPTree_2022postEE.root', isMC=True, nEvts = -1),
+        #'data_Run2022C': tnpSample('data_Run2022C', eos_Reco_Run3_PromptReco + '/TnPTree_EGamma_2022_Run2022E.root', lumi = 5.0104),
+        #'data_Run2022D': tnpSample('data_Run2022D', eos_Reco_Run3_PromptReco + '/TnPTree_EGamma_2022_Run2022F.root', lumi = 2.9700),
+        #'data_Run2022E': tnpSample('data_Run2022E', eos_Reco_Run3_PromptReco + '/TnPTree_EGamma_2022_Run2022E.root', lumi = 5.8070),
+        'data_Run2022F': tnpSample('data_Run2022F', eos_marti + '/2022DataFG/2022DataF.root', lumi = 17.7819),
+        'data_Run2022G': tnpSample('data_Run2022G', eos_marti + '/2022DataFG/2022DataG.root', lumi = 3.0828)
+
+        }
+
+Run3_2023 = {
+        'DYto2L-4Jets_madgraph_preBPix'    : tnpSample('', eos_marti + 'TnPTree_DYJetsToLL_M-50_TuneCP5_13p6TeV-madgraphMLM-pythia8_2022_DY_LO_postEE.root', isMC=True, nEvts = -1),
+        'DYto2L-4Jets_madgraph_postBPix'    : tnpSample('', eos_marti + 'TnPTree_DYJetsToLL_M-50_TuneCP5_13p6TeV-madgraphMLM-pythia8_2022_DY_LO_postEE.root', isMC=True, nEvts = -1),
+        'data_Run2023C': tnpSample('data_Run2023C', eos_Reco_Run3_PromptReco + '/TnPTree_EGamma_2022_Run2022E.root', lumi = 17.794),
+        'data_Run2023D': tnpSample('data_Run2023D', eos_Reco_Run3_PromptReco + '/TnPTree_EGamma_2022_Run2022F.root', lumi = 9.451)
+        }
+
+# TEST with Andro's samples
+Run3_Andro = {
+        'DYpreEE'    : tnpSample('DYpreEE', eos_marti + 'DY_madgraph_preEE.root', isMC=True, nEvts = -1),
+        'data_Run3C': tnpSample('data_Run3C', eos_marti + '/Run2022C_merged.root', lumi = 5.0104),
+        'data_Run3D': tnpSample('data_Run3D', eos_marti + '/Run2022D_merged.root', lumi = 2.9700)
         }
